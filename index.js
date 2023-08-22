b81efb54b62814a2c6beba10d802914e310254cc
Varathon Thach <142264466+Varathac@users.noreply.github.com>
2023-08-22 15:52:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 x
-app.use(express.static(__dirname + "/docs"));
+app.use(express.static(__dirname + "/Varathac.github.io"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
